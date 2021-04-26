a394277bd1ad52a3a2341739f3c86a93342bf901
Shailendra Singh <shaile.martin@gmail.com>
2021-04-26 19:56:32 +0530

    main

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads'))); 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3009);
 }
 bootstrap();
