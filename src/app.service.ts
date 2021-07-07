97b5b8602b2e165eddde935b50e73401255bf69b
UI Developer <shaile.martin@gmail.com>
2021-07-07 23:29:12 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Welome to the Azure App ! ${process.env.MONGODB_URI}`;
+    return `Welome to the Azure App @ ${process.env.MONGODB_URI} and ${process.env.PORT}`;
   }
 }
