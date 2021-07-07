f3726a6c81594184f65c2673d510a2bb3969f99f
UI Developer <shaile.martin@gmail.com>
2021-07-07 22:08:20 +0530

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Welome to the Az App !');
+      expect(appController.getHello()).toBe('Welome to the Azure App !');
     });
   });
 });
