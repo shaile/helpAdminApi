fef559e1d646c9edd0ffa549248d870c33710957
UI Developer <shaile.martin@gmail.com>
2021-07-07 22:03:16 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welome to the App!';
+    return 'Welome to the Az App !';
   }
 }
